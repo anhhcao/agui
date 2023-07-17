43375da003058e9fbbf0ecf1cbc07b8d6b660037
Peter Teuben <teuben@gmail.com>
2023-07-17 07:46:26 -0400

    change logic inside of loop

--- arun1.py
+++ arun1.py
@@ -23,6 +23,7 @@ def read_athinput(fname, mode=1):
     """
     lines = open(fname).readlines()
     Qall = False    # True causes too many, and this crashes tkrun
+    
     print("# Parsing %d lines in %s" % (len(lines),fname))
     ngui = 0
 
@@ -32,38 +33,39 @@ def read_athinput(fname, mode=1):
     for line in lines:
         line = line.strip()
         # print(line)
-        if len(line) == 0: continue
+        if len(line) == 0:
+            continue
         if line[0] == '<':
             key1 = line[1:]
             i = key1.find('>')
             key1 = key1[:i]
-        else:
-            #   "key = val    # help   #> tkrun"
-            word = line.split()
-            if len(word) < 3:
-                continue
-            if word[1] != '=':
-                print("Parsing error, not finding = in: ",line)
-                continue
-            key2 = word[0]
-            val2 = word[2]
-            i1 = line.find('#')
-            help2 = line[i1+1:].strip()
-            i2 = help2.find('#>')
-            if i2 > 0:
-                gui2 = help2[i2+2:].strip().split()
-                help2 = help2[:i2]
-                ngui = ngui + 1
-                if len(gui2) == 1:
-                    keys.append([key1,key2,val2,help2,gui2[0],''])
-                else:
-                    keys.append([key1,key2,val2,help2,gui2[0],gui2[1]])   
-                #print('%s=%s\\n%s#>%s' % (key2,val2,help2,gui2))
+            continue
+        # now we expect:
+        #   "key = val    # help   #> tkrun"
+        word = line.split()
+        if len(word) < 3:
+            continue
+        if word[1] != '=':
+            print("Parsing error, not finding = in: ",line)
+            continue
+        key2 = word[0]
+        val2 = word[2]
+        i1 = line.find('#')
+        help2 = line[i1+1:].strip()
+        i2 = help2.find('#>')
+        if i2 > 0:
+            gui2 = help2[i2+2:].strip().split()
+            help2 = help2[:i2]
+            ngui = ngui + 1
+            if len(gui2) == 1:
+                keys.append([key1,key2,val2,help2,gui2[0],''])
             else:
-                if Qall:
-                    keys.append([key1,key2,val2,help2,'ENTRY',''])
-                continue
-    # print("# DEBUG",keys)
+                keys.append([key1,key2,val2,help2,gui2[0],gui2[1]])   
+            #print('%s=%s\\n%s#>%s' % (key2,val2,help2,gui2))
+        else:
+            # not a GUI line
+            if Qall:
+                keys.append([key1,key2,val2,help2,'ENTRY',''])
     print("# classic tkrun output")
     for i in range(len(keys)):
         k = keys[i]
