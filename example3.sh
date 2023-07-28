2dce4ba1bf51b854aa0bb6a800adf5d8d3d4405d
Peter Teuben <teuben@gmail.com>
2023-07-27 21:11:21 -0400

    new style

--- example3.sh
+++ example3.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/env bash
 #
-# new 2023 style bash - works with tkrun now
+# new 2023 style bash 
 
- file1=foo       # help for input file1          #> IFILE file1=
- file2=bar       # help for output file2         #> OFILE file2=
-  dir3=fum       # help for input dir3           #> IDIR  dir3=
-  dir4=baz       # help for output dir4          #> ODIR  dir4=
- hello=world     # help for text entry hello     #> ENTRY hello=world
-     a=1         # help for a, between 0 and 2   #> SCALE a=1       0:2:0.1
-     b=2         # help for b, pick 1, 2 or 3    #> RADIO b=2       0,1,2
-     c=3         # help for c, check any of 6    #> CHECK c=3       0,1,2,a,b,c
+ file1=foo       # help for input file1          #> IFILE
+ file2=bar       # help for output file2         #> OFILE
+  dir3=fum       # help for input dir3           #> IDIR 
+  dir4=baz       # help for output dir4          #> ODIR 
+ hello=world     # help for text entry hello     #> ENTRY
+     a=1         # help for a, between 0 and 2   #> SCALE       0:2:0.1
+     b=2         # help for b, pick 1, 2 or 3    #> RADIO       0,1,2
+     c=3,c       # help for c, check any of 6    #> CHECK       0,1,2,a,b,c
 
 #
 for arg in "$@"; do
