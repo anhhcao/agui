fde2e2d8e3bf384d3a40ef29a37fd8363f3ef1a0
anhhcao <anhhcao@outlook.com>
2023-08-01 03:41:33 -0400

    dark theme

--- pysg_run.py
+++ pysg_run.py
@@ -8,8 +8,12 @@ from importlib import import_module
 cwd = getcwd()
 
 # fonts used in the GUI
-fstd = ('arial', 10)
-fstd_bold = ('arial', 10, 'bold')
+fstd = ('Helvetica', 10)
+fstd_bold = ('Helvetica', 10, 'bold')
+
+# background colors used in the GUI
+bgstd = '#2a2e32'
+bgstd2 = '#23272b'
 
 sliders = {}
 
@@ -28,11 +32,13 @@ if args.tk:
     primary = 'PySimpleGUI'
     backup = 'PySimpleGUIQt'
 
-try:
+'''try:
     sg = import_module(primary)
 except:
     sg = import_module(backup)
 
+'''
+import PySimpleGUIQt as sg
 # removes the trailing zeroes then the dot from a string float x, then returns an int
 # utility function used by buidd_layout
 def rm_dot(x):
@@ -48,26 +54,28 @@ def rm_dot(x):
 
 def build_layout(data, info):
     global cwd
-    layout = [[sg.Text('Problem:', font=fstd_bold), sg.Stretch(), sg.Text(info['problem'])]]
+    layout = [[sg.Text('Problem:', font=fstd_bold, background_color=bgstd), sg.Stretch(), sg.Text(info['problem'], background_color=bgstd)]]
     reference = info['reference']
     if reference: # empty strings are falsy
         # in the future, go to the link and get the abstract if possible
-        layout.append([sg.Text('Reference:', font=fstd_bold), sg.Stretch(), sg.Text(info['reference'])])
+        layout.append([sg.Text('Reference:', font=fstd_bold, background_color=bgstd), sg.Stretch(), sg.Text(info['reference'], background_color=bgstd)])
     else:
-        layout.append([sg.Text('Reference:', font=fstd_bold), sg.Stretch(), sg.Text('N/A')])
-    layout.extend([[sg.Text('Output directory:', font=fstd_bold, tooltip='The directory where the output files will be dumped'), 
+        layout.append([sg.Text('Reference:', font=fstd_bold, background_color=bgstd), sg.Stretch(), sg.Text('N/A', background_color=bgstd)])
+    layout.extend([[sg.Text('Output directory:', font=fstd_bold, tooltip='The directory where the output files will be dumped', background_color=bgstd), 
                         sg.In(size=(25, 0.75), 
                             enable_events=True, 
                             default_text=cwd, 
-                            key='output-dir'),
-                        sg.FolderBrowse(initial_folder=cwd)],
-                  [sg.Text('Parameters:', font=fstd_bold)]])
+                            key='output-dir',
+                            background_color=bgstd2,
+                            text_color='white'),
+                        sg.FolderBrowse(initial_folder=cwd, button_color=('white', bgstd2))],
+                  [sg.Text('Parameters:', font=fstd_bold, background_color=bgstd)]])
     for k in data:
         e = data[k]
         # use this if removing the prefix and underscore is desired
         # row = [sg.Text(match('.*_(.+)', k).group(1))] 
         # otherwise use
-        row = [sg.Text(k, tooltip=e['help'][1:].strip()), sg.Stretch()] # push to align right
+        row = [sg.Text(k, tooltip=e['help'][1:].strip(), background_color=bgstd), sg.Stretch()] # push to align right
         if e['gtype'] == 'SCALE':
             # getting scale params
             # min:max:increment?
@@ -82,7 +90,7 @@ def build_layout(data, info):
                     'key':e['value']+'_display',
                     'factor':round(scaled_default / float(e['value']))
                 }
-                row.append(sg.Text(float(e['value']), key=sliders[k]['key']))
+                row.append(sg.Text(float(e['value']), key=sliders[k]['key'], background_color=bgstd))
             # rm_dot only does anything significant if we are using qt
             row.append(sg.Slider(
                 range=(rm_dot(m.group(1)), rm_dot(m.group(2))),
@@ -90,7 +98,8 @@ def build_layout(data, info):
                 default_value=scaled_default,
                 enable_events=True,
                 key=k,
-                orientation='horizontal'
+                orientation='horizontal',
+                background_color=bgstd
             ))
         elif e['gtype'] == 'ENTRY':
             # entry = text box
@@ -99,21 +108,21 @@ def build_layout(data, info):
             row.append(sg.Input(
                 e['value'], 
                 enable_events=True, 
-                #expand_x=True, 
                 key=k,
-                #justification='right'
-                size=(20, 0.75)
+                size=(20, 0.75),
+                background_color=bgstd2,
+                text_color='white'
             ))
         elif e['gtype'] == 'RADIO':
             # number of options is not predetermined, so can't use regex
             for o in e['gparams'].split(','):
-                row.append(sg.Radio(o, k, key=k+o, default= o == e['value']))
+                row.append(sg.Radio(o, k, key=k+o, default= o == e['value'], background_color=bgstd))
         else:
             print('GUI type %s not implemented' % e['gtype'])
             exit()
         layout.append(row)
     # add buttons to run/quit/help
-    layout.extend([[sg.Text()], [sg.Button('Run', key='run'), sg.Button('Quit', key='quit'), sg.Button('Help', key='help')]])
+    layout.extend([[sg.Text(background_color=bgstd)], [sg.Button('Run', key='run', button_color=('white', bgstd2)), sg.Button('Quit', key='quit', button_color=('white', bgstd2)), sg.Button('Help', key='help', button_color=('white', bgstd2))]])
     return layout
 
 # collects the values from the GUI and builds the athena command
@@ -144,7 +153,7 @@ def run(input_file, output_dir, data, values):
 # builds and displays a new window containing only the athena command
 def display_output(s):
     if s:
-        window = sg.Window('Athena Output', [[sg.Text(s)]], font=fstd)
+        window = sg.Window('Athena Output', [[sg.Text(s, background_color=bgstd)]], font=fstd, background_color=bgstd)
         while True:
             event, _ = window.read()
             if event == sg.WIN_CLOSED:
@@ -153,10 +162,10 @@ def display_output(s):
 
 # builds and displays a new window containing the help information
 def display_help(data):
-    layout = [[sg.Text('Output directory:', font=fstd_bold), sg.Text('The directory where the output files will be dumped'), sg.Stretch()]]
+    layout = [[sg.Text('Output directory:', font=fstd_bold, background_color=bgstd), sg.Text('The directory where the output files will be dumped', background_color=bgstd), sg.Stretch()]]
     for k in data:
-        layout.append([sg.Text(k + ':', font=fstd_bold), sg.Text(data[k]['help'][1:].strip()), sg.Stretch()])
-    window = sg.Window('Help', layout, font=fstd)
+        layout.append([sg.Text(k + ':', font=fstd_bold, background_color=bgstd), sg.Text(data[k]['help'][1:].strip(), background_color=bgstd), sg.Stretch()])
+    window = sg.Window('Help', layout, font=fstd, background_color=bgstd)
     while True:
         event, _ = window.read()
         if event == sg.WIN_CLOSED:
@@ -164,8 +173,8 @@ def display_help(data):
     window.close()
 
 def display_conf_dir(dir_path):
-    layout = [[sg.Text(f'Directory {dir_path} does not exist. Create it?')], [sg.Button('Yes', key='yes'), sg.Button('No', key='no')]]
-    window = sg.Window('Directory Not Found', layout, font=fstd)
+    layout = [[sg.Text(f'Directory {dir_path} does not exist. Create it?', background_color=bgstd)], [sg.Button('Yes', key='yes', button_color=('white', bgstd2)), sg.Button('No', key='no', button_color=('white', bgstd2))]]
+    window = sg.Window('Directory Not Found', layout, font=fstd, background_color=bgstd)
     while True:
         event, _ = window.read()
         if event == 'no':
@@ -186,16 +195,14 @@ scale_factor = 30
 if args.tk:
     scale_factor = 40
 win_size = (500, len(inner_layout) * scale_factor)
-layout = [[sg.Column(inner_layout, size=win_size, scrollable=True)]]
+layout = [[sg.Column(inner_layout, size=win_size, scrollable=True, background_color=bgstd)]]
 
 # only allow verticle scroll for the tk version, otherwise a horizontal scroll bar will show up
 if args.tk:
     layout[0][0].VerticalScrollOnly = True
 
-sg.theme('DarkBlue13')
-
 # create the main window
-window = sg.Window('pysg_run', layout, size=win_size, font=fstd)
+window = sg.Window('pysg_run', layout, size=win_size, font=fstd, background_color=bgstd2)
 
 # primary event loop
 while True:
